c1188716a1225d33a4502cf82e3024aa75e25b0d
Victor Teo <victor.teo@fillmula.com>
2026-03-14 16:25:16 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@
 import Foundation
 import PackageDescription
 
-var sources = ["src/parser.c", "src/scanner.c"]
-
 let package = Package(
     name: "TreeSitterPython",
     products: [
@@ -18,7 +16,7 @@ let package = Package(
             name: "TreeSitterPython",
             dependencies: [],
             path: ".",
-            sources: sources,
+            sources: ["src/parser.c", "src/scanner.c"],
             resources: [
                 .copy("queries")
             ],
